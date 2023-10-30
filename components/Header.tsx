1bf45b84cbfcbd961521d0c915cbdac20d698a08
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-10-30 22:54:54 +0700

    Create a header component

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="z-[99] relative">
+      <div className="top-0 left-1/2 h-10 w-80 shadow-lg shadow-black/[0.03] rounded-full bg-red-500 text-green-600">
+        Header
+      </div>
+    </header>
+  );
+}

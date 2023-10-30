2b9a168998027371da1da86a5a4c139e8fa0d5ab
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-10-31 00:10:04 +0700

    Spacing, add styles, Link to li elements

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,23 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="flex justify-center">
       <nav
-        className="flex rounded-full bg-white/90 px-3 
+        className="flex justify-evenly rounded-full bg-white/90 px-3 
       font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 
-      ring-1 ring-zinc-900/5 backdrop-blur my-8 w-96 h-9"
+      ring-1 ring-zinc-900/5 backdrop-blur my-8 w-96 h-9
+      pointer-events-auto"
       >
-        <li>About</li>
-        <li>Projects</li>
-        <li>About</li>
+        <li className="hover:text-green-500 list-none my-1">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:text-green-500 list-none my-1">
+          <Link href="projects">Projects</Link>
+        </li>
+        <li className="hover:text-green-500 list-none my-1">
+          <Link href="/about">About</Link>
+        </li>
       </nav>
     </header>
   );

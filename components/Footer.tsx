6fcc394891aed4c1c3f1d18533b8b40493c48cb4
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-10-31 21:28:59 +0700

    Create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="mt-32 border-t border-zinc-100 pb-16 pt-10">
+        <div className="flex justify-end lg:px-8">
+          <div>
+            <p className="text-sm text-zinc-400 px-4 sm:px-8 lg:px-12">
+              © 2023 Tin Dejphachon. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
